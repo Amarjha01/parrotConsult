0992bd9a19e5d5752233c93121355a4451f663c0
Amar <amarjha880@gmail.com>
2025-07-31 20:02:52 +0530

    minor bug fix

--- client/src/pages/ClientDashboard.jsx
+++ client/src/pages/ClientDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SideBar from '../components/userDashboard/SideBar';
 import { Outlet } from 'react-router-dom';
-import { Menu, X } from 'lucide-react'; // or use any icon lib
+import { Menu, X } from 'lucide-react';
 
 const ClientDashboard = () => {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
@@ -12,6 +12,7 @@ const ClientDashboard = () => {
 
   return (
     <div>
+      {/* Mobile Header with Hamburger */}
       <div className="lg:hidden flex justify-end items-center p-4 bg-white shadow">
         <button onClick={handleNavbarOpen}>
           {isNavbarOpen ? <X size={24} /> : <Menu size={24} />}
@@ -20,7 +21,15 @@ const ClientDashboard = () => {
 
       <div className="lg:flex">
         <SideBar isNavbarOpen={isNavbarOpen} handleNavbarOpen={handleNavbarOpen} />
-        <div className="lg:flex-1 bg-gray-50 p-6 absolute h-[100vh] w-[100vw]">
+
+        {/* Content Area */}
+        <div
+          className={`
+            p-6 bg-gray-50 
+            absolute w-[100vw] h-[100vh] 
+            lg:static lg:w-auto lg:h-auto lg:flex-1
+          `}
+        >
           <Outlet />
         </div>
       </div>
